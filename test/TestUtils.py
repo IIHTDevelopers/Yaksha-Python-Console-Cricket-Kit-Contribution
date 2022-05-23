6df5aa4f4ad432823796cc43ae98dd52e752d905
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2022-05-23 21:51:11 +0530

    Update TestUtils.py

--- test/TestUtils.py
+++ test/TestUtils.py
@@ -1,5 +1,5 @@
-from test.TestResults import TestResults
-from test.TestCaseResultDto import TestCaseResultDto
+from notesapp.test.TestResults import TestResults
+from notesapp.test.TestCaseResultDto import TestCaseResultDto
 import json
 import requests
 
